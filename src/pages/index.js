4e540b7ebc82643ec26bfff4f2f1276abec2e6f2
murgatt <12082276+murgatt@users.noreply.github.com>
2021-10-18 14:01:58 +0200

    canvas manipulation WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
+import circle from '../images/circle.png';
 
 const IndexPage = () => {
+    const canvasRef = useRef(null);
+    const [ctx, setCtx] = useState();
     const [fileUrl, setFileUrl] = useState();
 
     const handleChange = event => {
+        const ctx = canvasRef.current.getContext('2d');
         const file = event.target.files[0];
-        setFileUrl(URL.createObjectURL(file));
+        if (file) {
+            const url = URL.createObjectURL(file);
+            setFileUrl(url);
+
+            const img = new Image();
+            img.src = url;
+            img.onload = () => {
+                ctx.drawImage(img, 0, 0, 400, 400);
+            }
+        }
     }
+
+    useEffect(() => {
+        setCtx(canvasRef.current.getContext('2d'));
+
+        if (ctx) {
+            const frame = new Image();
+            frame.src = circle;
+            frame.onload = () => {
+                ctx.drawImage(frame, 0, 0, 400, 400);
+            }
+        }
+    });
 
   return (
     <main>
         <title>Home Page</title>
         <input type="file" onChange={handleChange} accept="image/*" />
-        <img src={fileUrl} alt="Profile Picture" />
+        <canvas ref={canvasRef} width="400" height="400" />
     </main>
   )
 }
